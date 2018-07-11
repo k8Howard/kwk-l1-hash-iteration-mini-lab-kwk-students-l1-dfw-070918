3e5b2ec9409bc04a96e606d1ded30b263a4fb908
k8Howard <sweetk8@me.com>
2018-07-11 19:47:49 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -34,5 +34,20 @@ end
 end
 
 def iterate_through_keys
-  
+    summer_olympics = {
+    :Sydney => "2000",
+    :Athens => "2004",
+    :Beijing => "2008",
+    :London => "2012",
+    :Atlanta => "1996"
+  }
+  upcased_city = []
+  summer_olympics.each do |key.value|
+    upcased_city << key.upcase
+  end
+  puts upcased_city
 end
+
+
+
+
